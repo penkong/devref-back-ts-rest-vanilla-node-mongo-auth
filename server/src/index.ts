58b40049f8e71a758c71e9662b9057009234feda
mazdak <nazemi.works@gmail.com>
2021-04-27 19:18:19 +0200

    docker - compose fix npm link

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,20 +4,13 @@ import { config } from './config'
 
 const { PORT, DBURL, DBNAME, MONGOUSER, MONGOPASS } = config
 
-console.log(config)
-console.log('hello')
-console.log(PORT)
-
 // // Connection URL
-
-if (!DBURL) throw new Error('dburl is wrong')
-if (!MONGOUSER) throw new Error('cred is wrong')
-if (!MONGOPASS) throw new Error('cred is wrong')
 
 const url = DBURL!
   .replace('<MONGOUSER>', MONGOUSER!)
   .replace('<MONGOPASS>', MONGOPASS!)
 
+console.log(url)
 const client = new MongoClient(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -30,6 +23,7 @@ async function main() {
   try {
     await client.connect()
     console.log('Connected successfully to server')
+    console.log(3)
   } catch (error) {
     console.log(error)
   }
