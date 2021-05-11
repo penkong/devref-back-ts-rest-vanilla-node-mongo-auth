c225ae8db63ea50040f247c28d4bc01f59b3abae
mazdak <nazemi.works@gmail.com>
2021-05-11 10:19:31 +0200

    more on

--- app/services/auth-service/src/util/validRoutes.const.ts
+++ app/services/auth-service/src/util/validRoutes.const.ts
@@ -1,6 +1,6 @@
 export const validRoutes = [
-  'POST/api/v1/auth/register',
-  'POST/api/v1/auth/login',
-  'GET/api/v1/auth/current-user',
-  'GET/api/v1/auth/logout'
+  'POST/v1/api/auth/register',
+  'POST/v1/api/auth/login',
+  'GET/v1/api/auth/current-user',
+  'GET/v1/api/auth/logout'
 ]
